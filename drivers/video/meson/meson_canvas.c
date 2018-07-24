527f50de590a816e3cfc53d631bf11ade1f57176
Neil Armstrong <narmstrong@baylibre.com>
2018-07-24 17:45:28 +0200

    video: Add Meson Video Processing Unit Driver
    This adds video output support for Amlogic GXBB/GXL/GXM chips.
    The supported ports are CVBS and HDMI (based on DW_HDMI).
    
    When using HDMI, only DMT modes are supported.
    
    There is support for simple-framebuffer (CONFIG_VIDEO_DT_SIMPLEFB)
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Jorge Ramire-Ortiz <jramirez@baylibre.com>
    Signed-off-by: Maxime Jourdan <mjourdan@baylibre.com>

--- drivers/video/meson/meson_canvas.c
+++ drivers/video/meson/meson_canvas.c
@@ -11,14 +11,14 @@
 /* DMC Registers */
 #define DMC_CAV_LUT_DATAL	0x48 /* 0x12 offset in data sheet */
 #define CANVAS_WIDTH_LBIT	29
-#define CANVAS_WIDTH_LWID	3
+#define CANVAS_WIDTH_LWID       3
 #define DMC_CAV_LUT_DATAH	0x4c /* 0x13 offset in data sheet */
-#define CANVAS_WIDTH_HBIT	0
-#define CANVAS_HEIGHT_BIT	9
-#define CANVAS_BLKMODE_BIT	24
+#define CANVAS_WIDTH_HBIT       0
+#define CANVAS_HEIGHT_BIT       9
+#define CANVAS_BLKMODE_BIT      24
 #define DMC_CAV_LUT_ADDR	0x50 /* 0x14 offset in data sheet */
-#define CANVAS_LUT_WR_EN	(0x2 << 8)
-#define CANVAS_LUT_RD_EN	(0x1 << 8)
+#define CANVAS_LUT_WR_EN        (0x2 << 8)
+#define CANVAS_LUT_RD_EN        (0x1 << 8)
 
 void meson_canvas_setup(struct meson_vpu_priv *priv,
 			u32 canvas_index, u32 addr,
